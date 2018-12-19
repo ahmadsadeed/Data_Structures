6df4d2a9ed2c17c0693b948483db07e049158387
Sadeed <sadeed.hakimi@yahoo.com>
2018-12-19 00:09:35 -0800

    added faster algorithm

--- week1/Programming-Assignment-1/tree_height/tree_height.cpp
+++ week1/Programming-Assignment-1/tree_height/tree_height.cpp
@@ -5,6 +5,8 @@
 #include <sys/resource.h>
 #endif
 
+using namespace std;
+
 class Node;
 
 class Node {
@@ -23,32 +25,48 @@ public:
     }
 };
 
+int getHeight(Node &node){
+// to print in order
+//    for (int i = 0; i < node.children.size(); i++) {
+//        getHeight(*node.children[i]);
+//    }
+    // 0  1 2 3 4 5 6 7 8 9 10
+    // 4 -1 4 1 1 3 5 6 7 8
+    // 4 -1 4 1 1 1 5 6 7 8
+    // -1 0 4 0 3
+    if (node.children.size() > 0) {
+        vector<int> v;
+        for (int i = 0; i < node.children.size(); i++) {
+            int max = getHeight(*node.children[i]);
+            v.push_back(max);
+        }
+        sort(v.begin(), v.end());
+
+        return 1 + v[v.size()-1];
+    } else if (node.children.size() == 0) {
+        return 0;
+    }
+    return 0;
+}
+
 
 int main_with_large_stack_space() {
     std::ios_base::sync_with_stdio(0);
-    int n = 5;
-//    std::cin >> n;
-    // 4 -1 4 1 1
-
+    int n;
+    std::cin >> n;
     std::vector<Node> nodes;
     nodes.resize(n);
+    int root = -1;
     for (int child_index = 0; child_index < n; child_index++) {
         int parent_index;
         std::cin >> parent_index;
         if (parent_index >= 0)
             nodes[child_index].setParent(&nodes[parent_index]);
+        else root = child_index;
         nodes[child_index].key = child_index;
     }
 
-    // Replace this code with a faster implementation
-    int maxHeight = 0;
-    for (int leaf_index = 0; leaf_index < n; leaf_index++) {
-        int height = 0;
-        for (Node *v = &nodes[leaf_index]; v != NULL; v = v->parent)
-            height++;
-        maxHeight = std::max(maxHeight, height);
-    }
-
+    int maxHeight = 1 + getHeight(nodes[root]);
     std::cout << maxHeight << std::endl;
     return 0;
 }
@@ -78,4 +96,5 @@ int main (int argc, char **argv)
 #endif
     return main_with_large_stack_space();
 }
+
 
